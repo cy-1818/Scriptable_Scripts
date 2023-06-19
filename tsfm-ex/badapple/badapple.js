434113ffe287d182506544cba944e1c056b56dc3
cy-1818 <104477638+cy-1818@users.noreply.github.com>
2023-06-19 18:40:17 +0900

    Add files via upload

--- tsfm-ex/badapple/badapple.js
+++ tsfm-ex/badapple/badapple.js
@@ -7,12 +7,30 @@ return(async function(){
       "tag":"pre",
       "edit":true
   }])
-  while(Date.now()<startTime+218000){
-    await Edit(appleNum, {
-      "style":"color:white",
-      "str":apple[Math.floor((Date.now()-startTime)/100)].join("<br/>")
-    });
+  var index = Math.floor((Date.now()-startTime)/100)
+  for(var n=0;n<29;n++){
+    await Print([{
+      "style":"color:white;margin:0;padding:0;",
+      "str":"",
+      "tag":"pre",
+      "edit":true
+    }])
   }
-  await DelNode(appleNum);
+  while(index!=apple.length){
+    for(var n=0;n<30;n++){
+      await Edit(appleNum+n, {
+        "style":"color:white;margin:0;padding:0;",
+        "str":apple[index][n].split("<").join("&lt")
+      });
+    }
+    index = Math.floor((Date.now()-startTime)/100)
+  }
+  for(var n=0;n<30;n++){
+    await DelNode(appleNum+n);
+  }
+  apple = null;
+  startTime = null;
+  appleNum = null;
+  index = null;
   return [];
 })()
