3e6000e5806da8cae9e7e2fda23243815e974adc
Yukí <104477638+cy-1818@users.noreply.github.com>
2023-07-26 11:14:43 +0900

    Add files via upload

--- tsfm-ex/tsfm-ex/get.js
+++ tsfm-ex/tsfm-ex/get.js
@@ -1,11 +1,35 @@
 return (async function(){
   var result = []
   try{
-    await f.writeString(formatPath(parameter[1], pass), await new Request(parameter[0]).loadString());
-    result = [{
-      "style":"",
-      "str":"success!"
-    }]
+    if(parameter[1]){
+      await f.writeString(formatPath(parameter[1], pass), await new Request(parameter[0]).loadString());
+      result = [{
+        "style":"",
+        "str":"success!"
+      }]
+    }else if(parameter[0] == "-v"){
+      result = [{
+        "style":"",
+        "str":"2.0"
+      }]
+    }else if(parameter[0] == "-h"){
+      result = [{
+        "style":"",
+        "str":"get [url] [fileName] : download file from url. "
+      }]
+    }else if(commands.show){
+      var list = (await new Request(parameter[0]).loadString()).split("\n")
+      list.unshift("-c");
+      result = await Command("show", list);
+    }else{
+      result = [{
+        "style":"color:#ff3333",
+        "str":"[ERROR] "
+      },{
+        "style":"color:#ff3333",
+        "str":"write file name. If you install \"show\", you can only download and look."
+      }]
+    }
   }catch(e){
     result = [{
       "style":"color:#ff3333",
