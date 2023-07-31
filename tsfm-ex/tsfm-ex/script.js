08c22612cc5810d8d656af56d4b31a070dc862a9
Yukí <104477638+cy-1818@users.noreply.github.com>
2023-07-31 11:24:17 +0900

    Add files via upload

--- tsfm-ex/tsfm-ex/script.js
+++ tsfm-ex/tsfm-ex/script.js
@@ -1,8 +1,23 @@
 return (async function(){
-	var urls = JSON.parse(f.readString(doc+"/tsfm-ex/urls.json"));
+    if(!fmi.fileExists(doci+"/tsfm-ex/scripts.json")){
+      var scripts = {}
+      var commandKeys = Object.keys(commands);
+      for(var n=0;n<commandKeys.length;n++){
+        scripts[commandKeys[n]] = {
+          "path":doci+commands[commandKeys[n]],
+          "type":"command"
+        }
+      }
+      await fmi.writeString(doci+"/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
+    }else{
+      scripts = JSON.parse(fmi.readString(doci+"/tsfm-ex/scripts.json"));
+    }
+	var urls = JSON.parse(fmi.readString(doci+"/tsfm-ex/urls.json"));
 	var urlList = {};
 	var result = [];
-	var tsfmex;
+	var notOnly = false;
+	var istr = "installed";
+	var ifstr = "install complete"
 	for(var n = 0;n<urls.length;n++){
 	  var rq = new Request(urls[n]);
 	  var rqjson = await rq.loadJSON();
@@ -25,41 +40,72 @@ return (async function(){
 		      }
 		    }
 		  break;
+		  case "list":
+		  case "-l":
+		    var nameList = Object.keys(scripts);
+		    for(var n=0;n<nameList.length;n++){
+		      result.push({
+		        "style":"display:block;",
+		        "str":nameList[n]
+		      });
+		    }
+		  break;
+		  case "update":
+		  case "-u":
+		    for(var n=0;n<Object.keys(scripts).length;n++){
+		      parameter.push(Object.keys(scripts)[n])
+		    }
+		    istr = "updated"
+		    ifstr = "update complete"
 		  case "install":
 		  case "-i":
+		    notOnly = true;
+		    var depended={};
+		  case "installOnly":
+		  case "-io":
 		    if(parameter[1]){
 		      for(var n=1;n<parameter.length;n++){
 		        if(urlList[parameter[n]]){
 		          var rq = new Request(urlList[parameter[n]].url);
 		          var rqstr = await rq.loadString();
-		          if(parameter[n]=="tsfm-ex"){
-		            tsfmex = rqstr;
-		            await Print([{
-		              "style":"color:green",
-		              "str":parameter[n]+" will be installed later"
-		            }])
-		            continue;
-		          }
+		          var newPath;
 		          switch(urlList[parameter[n]].type){
 		            case "command":
-		              await f.writeString(doc + "/tsfm-ex/" + urlList[parameter[n]].name, rqstr)
+		              newPath = doci + "/tsfm-ex/" + urlList[parameter[n]].name
 		              commands[parameter[n]]="/tsfm-ex/" + urlList[parameter[n]].name;
-		              await f.writeString(doc+"/tsfm-ex/commands.json", JSON.stringify(commands))
+		              await fmi.writeString(doci+"/tsfm-ex/commands.json", JSON.stringify(commands, null, "\t"))
 		            break;
 		            case "Scriptable":
-		              await f.writeString(doc + "/" + urlList[parameter[n]].name, rqstr)
+		              newPath = doci + "/" + urlList[parameter[n]].name
+		            break;
+		            case "addition":
+		              newPath = doci + "/tsfm-ex/" + urlList[parameter[n]].name
 		            break;
 		          }
-		          if(urlList[parameter[n]].dependence){
-		            for(var i=1;i<urlList[parameter[n]].dependence.length;i++){
+		          await fmi.writeString(newPath, rqstr)
+		          scripts[parameter[n]]={
+		            "path":newPath,
+		            "type":urlList[parameter[n]].type,
+		            "name":urlList[parameter[n]].name,
+		            "dependence":urlList[parameter[n]].dependence,
+		            "depended":[],
+		            "shortcuts":[]
+		          }
+		          await fmi.writeString(doci + "/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
+		          if(notOnly && urlList[parameter[n]].dependence){
+		            for(var i=0;i<urlList[parameter[n]].dependence.length;i++){
 		              if(!parameter.includes(urlList[parameter[n]].dependence[i])){
 		                parameter.push(urlList[parameter[n]].dependence[i])
 		              }
+		              if(depended[urlList[parameter[n]].dependence[i]] === undefined){
+		                depended[urlList[parameter[n]].dependence[i]] = [];
+		              }
+		              depended[urlList[parameter[n]].dependence[i]].push(parameter[n]);
 		            }
 		          }
 		          await Print([{
 		            "style":"",
-		            "str":parameter[n]+" was installed"
+		            "str":parameter[n]+" was "+istr
 		          }])
 		        }else{
 		          await Print([{
@@ -71,9 +117,14 @@ return (async function(){
 		          }])
 		        }
 		      }
+		      var dlist = Object.keys(depended);
+		      for(var n=0;n<dlist.length;n++){
+		        scripts[dlist[n]].depended = depended[dlist[n]].concat()
+		      }
+		      await fmi.writeString(doci + "/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
 		      result.push({
 		        "style":"",
-		        "str":"install complete"
+		        "str":ifstr
 		      })
 		    }else{
 		      result.push({
@@ -81,14 +132,48 @@ return (async function(){
 		        "str":"To install script, write \'script install [script name]\'."
 		      })
 		    }
+		    await fmi.writeString(doci+"/tsfm-ex/commands.json", JSON.stringify(commands, null, "\t"))
 		  break;
 		  case "delete":
 		  case "-d":
+		    notOnly = true
+		  case "deleteOnly":
+		  case "-do":
 		    for(var n=1;n<parameter.length;n++){
-		      if(commands[parameter[n]]){
-		        await f.remove(doc + commands[parameter[n]]);
-		        delete commands[parameter[n]];
-		        await f.writeString(doc+"/tsfm-ex/commands.json", JSON.stringify(commands))
+		      if(notOnly && scripts[parameter[n]].depended.length>0){
+		        await Print([{
+		            "style":"color:#ff3333",
+		            "str":"[ERROR] "
+		          },{
+		            "style":"",
+		            "str":parameter[n]+" is depended on other scripts. Use \"deleteOnly\" to delete."
+		          }])
+		      }else if(scripts[parameter[n]]){
+		        if(scripts[parameter[n]].type == "command"){
+		          delete commands[parameter[n]];
+		          await fmi.writeString(doci+"/tsfm-ex/commands.json", JSON.stringify(commands, null, "\t"))
+		        }
+		        var dependence = scripts[parameter[n]].dependence
+		        if(notOnly && dependence){
+		          for(var i=0;i<dependence.length;i++){
+		            if(scripts[dependence[i]] !== undefined){
+		              scripts[dependence[i]].depended.splice(scripts[dependence[i]].depended.indexOf(parameter[n]), 1)
+		              if(scripts[dependence[i]].depended.length === 0){
+		                if(!parameter.includes(dependence[i])){
+		                  parameter.push(dependence[i])
+		                }
+		              }
+		            }
+		          }
+		        }
+		        if(scripts[parameter[n]].shortcuts){
+		          for(var i=0;i<scripts[parameter[n]].shortcuts.length;i++){
+		            delete commands[scripts[parameter[n]].shortcuts[i]];
+		          }
+		        }
+		        await fmi.remove(scripts[parameter[n]].path);
+		        delete scripts[parameter[n]];
+		        await fmi.writeString(doci+"/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
 		        await Print([{
 		          "style":"",
 		          "str":parameter[n]+" was deleted"
@@ -111,30 +196,66 @@ return (async function(){
 		  case "addURL":
 		  case "-a":
 		    urls=urls.concat(parameter.slice(1))
-		    await f.writeString(doc+"/tsfm-ex/urls.json", JSON.stringify(urls))
+		    await f.writeString(doc+"/tsfm-ex/urls.json", JSON.stringify(urls, null, "\t"))
+		  break;
+		  case "removeURL":
+		  case "-r":
+		    urls.splice(urls.indexOf(parameter[1]), 1)
+		    await f.writeString(doc+"/tsfm-ex/urls.json", JSON.stringify(urls, null, "\t"))
+		  break;
+		  case "package":
+		  case "-p":
+		    scripts[parameter[1]] = {
+		      "path":formatPath(parameter[2], pass),
+		      "type":"command",
+		      "name":[parameter[2].split("/").pop()].name,
+		      "dependence":parameter.slice(3),
+		      "shortcuts":[]
+		    }
+		    commands[parameter[1]] = formatPath(parameter[2], pass).replace(doci, '')
+		    await fmi.writeString(doci+"/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
+		    await fmi.writeString(doci+"/tsfm-ex/commands.json", JSON.stringify(commands, null, "\t"))
+		  break;
+		  case "name":
+		  case "-n":
+		    commands[parameter[2]] = commands[parameter[1]]
+		    scripts[parameter[1]].shortcuts.push(parameter[2]);
+		    await fmi.writeString(doci+"/tsfm-ex/scripts.json", JSON.stringify(scripts, null, "\t"))
+		    await fmi.writeString(doci+"/tsfm-ex/commands.json", JSON.stringify(commands, null, "\t"))
 		  break;
 		  case "version":
 		  case "-v":
 		    result.push({
 		      "style":"",
-		      "str":"2.6"
+		      "str":"5.2"
 		    })
 		  break;
 		  case "help":
 		  case "-h":
 		  default:
-		    result.push({
-		      "style":"",
-		      "str":`
-install [script name] : You can install scripts.<br/>
-delete [script name]  : You can delete scripts.<br/>
-delete [file pass]    : You can delete files.<br/>
-search [String]       : You can search scripts.<br/>
-addURL [URL]          : You can add URL of JSON which includes script names and urls.<br/>
-version               : show version.<br/>
-help                  : show help.<br/>
-You can use -i, -d, -s, -a, -v, -h instead if install, delete, search, addURL, version, help.`.split("\n").join("")
-		    })
+		    var text = `
+install [script name]    : You can install scripts.
+delete [script name]     : You can delete scripts.
+search [String]          : You can search scripts.
+list                     : You can check installed scripts list.
+update                   : You can update installed scripts.
+addURL [URL]             : You can add URL of JSON which includes script names and urls.
+removeURL [URL]          : You can delete a URL.
+package [name] [path]    : You can make command from your local file.
+name [command] [shorcut] : You can make shortcut.
+version                  : show version.
+help                     : show help.
+installOnly              : install without dependences.
+deleteOnly               : install without dependences.
+You can use -i, -d, -s, -l, -u, -a, -r, -p, -n, -v, -h, -io, -do instead of them.`.split("\n");
+            for(var n=0;n<text.length;n++){
+		      result.push({
+		        "style":"display:block",
+		        "tag":"pre",
+                "notPara":true,
+		        "str":text[n]
+		      });
+		    }
 		  break;
 		}
 	}catch(e){
@@ -143,14 +264,5 @@ You can use -i, -d, -s, -a, -v, -h instead if install, delete, search, addURL, v
 	    "str":`[${e.name}] ${e.message}`
 	  }]);
 	}
-	if(tsfmex){
-	  elements.run=`
-return (async function(){
-  await f.writeString(doc + "/tsfm-ex.js", parameter)
-  return 0;
-}`
-	  elements.runparameter=tsfmex;
-	}else{
-	  return result;
-	}
+	return result;
 })()
